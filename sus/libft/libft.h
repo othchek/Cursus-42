f0ad19e2cf683c90137a7eabb50e346239e52c89
othchek <59626158+othchek@users.noreply.github.com>
2022-10-13 02:58:34 +0100

    commit

--- sus/libft/libft.h
+++ sus/libft/libft.h
@@ -1,6 +1,9 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#ifndef LIBFT_H
+#define LIBFT_H
+
 int	ft_isalnum(int c);
 int ft_atoi(const char *str);
 int	ft_isalpha(int c);
@@ -12,6 +15,8 @@ size_t	ft_strlen(const char *s);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
-char	*ft_strrchr(const char *s, int c);
+char    *ft_strrchr(const char *s, int c);
 void *ft_memset(void *b, int c, size_t len);
+
+#endif
 
--- sus/libft/main.c
+++ sus/libft/main.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+
 #include "libft.h"
 
 int main()
 {
     char myName[] = "papay";
-    char random[] = "--+152";
-    char random1[] = "-16";
+    char random[] = "papay";
+    char random1[] = "papay";
     printf(">> The begining of ft_atoi main >>\n");
-    printf(">>>> First test : %d\tSecond test : %d >>>>\n", ft_atoi(random), ft_atoi(random1));
+    printf(">>>> First test : %zu\tSecond test : %zu >>>>\n", ft_strlen(random), ft_strlen(random1));
     printf(">> The end of ft_atoi main >>\n");
     printf(">> The begining of ft_strrchr main >>\n");
     printf(">>>> First test : %s\tSecond test : %s >>>>\n", ft_strrchr(myName, 'p'), ft_strrchr(myName, 'a'));
