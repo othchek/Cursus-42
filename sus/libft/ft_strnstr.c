44dfe60aca921f7363f02a3fc9ff4a3e49aa4782
othchek <59626158+othchek@users.noreply.github.com>
2022-10-14 00:30:14 +0100

    commited

--- sus/libft/ft_strnstr.c
+++ sus/libft/ft_strnstr.c
@@ -6,12 +6,10 @@
 /*   By: otchekai <otchekai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 10:18:08 by otchekai          #+#    #+#             */
-/*   Updated: 2022/10/14 00:28:47 by otchekai         ###   ########.fr       */
+/*   Updated: 2022/10/14 00:29:47 by otchekai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
 #include "libft.h"
 
 char *ft_strnstr(const char *haystack, const char *needle, size_t len)
@@ -35,11 +33,4 @@ char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 		if(haystack[i] != needle[j])
 		return(0);
 	return(0);
-}
-int main()
-{
-	char haystack[] = "papay";
-	char needle[] = "a";
-	printf("%s\n", ft_strnstr(haystack, needle, 5));
-	printf("%s", strnstr(haystack, needle, 5));	
 }
