19fe590b55c423f9081faa0f1842f1bf26a33754
othchek <59626158+othchek@users.noreply.github.com>
2022-10-16 09:01:24 +0100

    commit

---
+++ sus/libft/ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otchekai <otchekai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/14 03:24:22 by otchekai          #+#    #+#             */
+/*   Updated: 2022/10/16 08:59:09 by otchekai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	int i;
+	unsigned char *str;
+
+	str = (unsigned char *)s;
+	i = 0;
+	while(i < n)
+	{
+		if(str[i] == c)
+			return(&str[i]);	
+		i++;
+	}
+	return(0);
+}
