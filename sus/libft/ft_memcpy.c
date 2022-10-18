e3a697350391b52e34b6cf6b7174a6e0e510331a
othchek <59626158+othchek@users.noreply.github.com>
2022-10-18 13:10:32 +0100

    asd

--- sus/libft/ft_memcpy.c
+++ sus/libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: otchekai <otchekai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 10:06:54 by otchekai          #+#    #+#             */
-/*   Updated: 2022/10/18 11:10:20 by otchekai         ###   ########.fr       */
+/*   Updated: 2022/10/18 13:09:57 by otchekai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,12 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	str = (char *) src;
 	res = (char *) dst;
 	i = 0;
+	if (str == 0 && res == 0)
+		return (0);
 	while (i < n)
 	{
 		res[i] = str[i];
 		i++;
-	}
-	while (i < n)
-	{
-		res[i] = '\0';
-			i++;
 	}
 	return (res);
 }
