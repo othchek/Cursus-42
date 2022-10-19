d6db36dfc0df000eb9f2d43afefd25c7aaffa4dd
othchek <59626158+othchek@users.noreply.github.com>
2022-10-19 10:36:38 +0100

    asd

--- sus/libft/ft_calloc.c
+++ sus/libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: otchekai <otchekai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 08:21:38 by otchekai          #+#    #+#             */
-/*   Updated: 2022/10/19 10:33:13 by otchekai         ###   ########.fr       */
+/*   Updated: 2022/10/19 10:36:17 by otchekai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void	*ft_calloc(size_t count, size_t size)
 	ptr = malloc(count * sizeof(char));
 	if (!ptr)
 		return (0);
-	bzero(ptr, size);
+	ft_bzero(ptr, size);
 	return (ptr);
 }
