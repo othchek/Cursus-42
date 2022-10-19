fd2cf11e69fcd009cf72405d0142bf5d3673953b
othchek <59626158+othchek@users.noreply.github.com>
2022-10-19 10:39:18 +0100

    staged

--- sus/libft/ft_calloc.c
+++ sus/libft/ft_calloc.c
@@ -6,11 +6,12 @@
 /*   By: otchekai <otchekai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 08:21:38 by otchekai          #+#    #+#             */
-/*   Updated: 2022/10/19 10:36:17 by otchekai         ###   ########.fr       */
+/*   Updated: 2022/10/19 10:37:16 by otchekai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
