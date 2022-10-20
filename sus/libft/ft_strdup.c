891411bde6a4ddc3fc4d32d43ec7ac0a4ff8a763
othchek <59626158+othchek@users.noreply.github.com>
2022-10-20 07:41:12 +0100

    added

--- sus/libft/ft_strdup.c
+++ sus/libft/ft_strdup.c
@@ -1,29 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: otchekai <otchekai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/14 03:24:22 by otchekai          #+#    #+#             */
-/*   Updated: 2022/10/20 07:40:10 by otchekai         ###   ########.fr       */
+/*   Created: 2022/10/19 11:04:58 by otchekai          #+#    #+#             */
+/*   Updated: 2022/10/19 11:05:25 by otchekai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
-{
-	size_t			i;
-	char			*str;
-
-	str = (char *)s;
-	i = 0;
-	while (i < n)
-	{
-		if (str[i] == (char) c)
-			return (&str[i]);
-		i++;
-	}
-	return (0);
-}
+char *ft_strdup(const char *s1)
