eafe776f425ccb93cc4e3930991d35430eb44672
othchek <59626158+othchek@users.noreply.github.com>
2022-10-28 15:46:11 +0100

    added

--- sus/libft/ft_strnstr.c
+++ sus/libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: otchekai <otchekai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 10:18:08 by otchekai          #+#    #+#             */
-/*   Updated: 2022/10/28 14:07:45 by otchekai         ###   ########.fr       */
+/*   Updated: 2022/10/28 15:45:15 by otchekai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,32 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	size_t	k;
 
-	i = 0;
-	j = 0;
-	k = 0;
-	if (!haystack && !len)
-		return (NULL);
-	if (needle[j] == '\0')
+	if (!*needle)
 		return ((char *)haystack);
+	i = 0;
+	if (!len)
+		return (NULL);
 	while (i < len && haystack[i] != '\0')
 	{
-		while (i < len && needle[j] == haystack[i])
-		{
-			j++;
-			i++;
-			if (needle[j] == '\0')
-				return ((char *)haystack + k);
-		}
+		
 		j = 0;
-		i++;
-		k++;
+		while (haystack[i + j] == needle[j] && i + j < len)
+		{
+			if (!needle[j + 1])
+			{
+				return ((char *)haystack + i);
+			}
+			j++;	
+		}
+	i++;
+		
 	}
 	return (0);
 }
 
 // int main()
 // {
-// 	char hay [] = "lorem ipsum lodorr";
-// 	char ned [] = "lodorr"
+// 	char hay [] = "lorem ipsum dolor";
+// 	char ned [] = "dolor"
 // }
