5b694e4ad401c7f7c25b2bc09072fb78489c5d33
othchek <59626158+othchek@users.noreply.github.com>
2022-12-03 13:55:32 +0100

    added

--- sus/get_next_line/get_next_line.c
+++ sus/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: otchekai <otchekai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 11:33:34 by otchekai          #+#    #+#             */
-/*   Updated: 2022/11/30 23:28:20 by otchekai         ###   ########.fr       */
+/*   Updated: 2022/12/03 13:54:25 by otchekai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@ char	*ft_read(int fd, char *buffer)
 	while (main > 0 && !ft_strchr(ptr, '\n'))
 	{
 		main = read(fd, ptr, BUFFSIZE);
-		if (main == -1 || (main == 0 && !ptr))
+		if (main < 0 || (main == 0 && !buffer)
+			|| (main == 0 && buffer[0] == 0))
 			return (free(ptr), free(buffer), NULL);
 		ptr[main] = '\0';
 		buffer = ft_strjoin(buffer, ptr);
@@ -53,7 +54,7 @@ char	*ft_morow(char *str)
 		ptr[sec] = str[sec];
 		sec++;
 	}
-	ptr[main] = '\0';
+	ptr[sec] = '\0';
 	return (ptr);
 }
 
@@ -73,7 +74,7 @@ char	*ft_morew(char *str)
 		return (free(str), NULL);
 	ptr = malloc((size - main) + 1);
 	if (!ptr)
-		return (free(str), free(ptr), NULL);
+		return (free(str), NULL);
 	main++;
 	while (str[main])
 		ptr[sec++] = str[main++];
@@ -87,7 +88,11 @@ char	*get_next_line(int fd)
 	static char		*stats;
 	char			*notsta;
 
+	if (fd < 0 || BUFFSIZE <= 0)
+		return (NULL);
 	stats = ft_read(fd, stats);
+	if (!stats)
+		return (NULL);
 	if (!*stats)
 		return (free(stats), NULL);
 	notsta = ft_morow(stats);
