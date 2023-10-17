b445d7cc8912e2816385bc8cd5046d64dbfbb51d
Othman Chekairi <otchekai@e2r4p14.1337.ma>
2023-10-17 21:32:15 +0100

    finished

--- sus/CPP/CPP01/ex04/main.cpp
+++ sus/CPP/CPP01/ex04/main.cpp
@@ -1,13 +1,10 @@
 #include "sed.hpp"
 
 int main(int ac, char **av) {
-    if (ac == 4)
-    {
-        std::ifstream outFile(av[1]);
-        if (!outFile.is_open())
-            return std::cout << RED << "File does not exist" << RESET << std::endl, 0;
-        file_practice(av[1], av[2], av[3]);
-    }
-    else
-        std::cout << RED << "Invalid Arguments" << RESET << std::endl;
+    if (ac != 4)
+        return std::cout << RED << "Invalid Arguments" << RESET << std::endl, 0;
+    std::ifstream outFile(av[1]);
+    if (!outFile.is_open())
+        return std::cout << RED << "File does not exist" << RESET << std::endl, 0;
+    file_practice(av[1], av[2], av[3]);
 }
--- sus/CPP/CPP01/ex06/Harl.cpp
+++ sus/CPP/CPP01/ex06/Harl.cpp
@@ -10,27 +10,43 @@ void    Harl::info(void)
     std::cout << ORANGE << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << RESET << std::endl;
 }
 
+
 void    Harl::warning(void)
 {
     std::cout << ORANGE << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << RESET << std::endl;
 }
+
 void    Harl::error(void)
 {
     std::cout << ORANGE << "This is unacceptable! I want to speak to the manager now." << RESET <<std::endl;
 }
 
 void Harl::complain(std::string level) {
-    void	(Harl::*ptr[5])(void);
-    ptr[1] = &Harl::debug;
-    ptr[2] = &Harl::info;
-    ptr[3] = &Harl::warning;
-    ptr[4] = &Harl::error;
-
-    const char *converter = level.c_str();
-    int purpose = atoi(converter);
-    if (purpose >= 1 && purpose <= 4) {
-        (this->*ptr[purpose])();
-    } else {
-        std::cout << RED << "Invalid level: " << level << RESET << std::endl;
+    std::string array[4] = {
+        "DEBUG",
+        "INFO",
+        "WARNING",
+        "ERROR"
+    };
+    int purpose = 0;
+    for (int i = 0; i < 4; i++) {
+        if (array[i] == level) {
+            purpose = i + 1;
+            break;
+        }
+    }
+    switch (purpose) {
+        case 1:
+            debug();
+        case 2:
+            info();
+        case 3:
+            warning();
+        case 4:
+            error();
+            break;
+        default:
+            std::cout << RED << "[ Probably complaining about insignificant problems ]" << level << RESET << std::endl;
+            break;
     }
 }
