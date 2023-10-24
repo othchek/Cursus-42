a5966e6c6a3cd8bd70f58cd5d12b0aa1830172dc
Othman Chekairi <otchekai@e1r10p3.1337.ma>
2023-10-24 17:38:13 +0100

    New files

---
+++ sus/CPP/CPP02/ex02/Fixed.hpp
@@ -0,0 +1,42 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#define BLUE      "\x1b[34m"
+#define ORANGE    "\x1b[33m"
+#define RESET     "\x1b[0m"
+#define PURPLE    "\x1b[96m"
+#define GREEN     "\x1b[32m"
+#define RED       "\x1b[1;31m"
+
+#include <iostream>
+
+class Fixed {
+    private :
+        int stash;
+        static const int fractionalbits;
+    public :
+        Fixed();
+        ~Fixed();
+        Fixed(const int num);
+        Fixed(const float num);
+        Fixed(const Fixed &obj);
+
+        bool operator==(const Fixed& obj) const;
+        bool operator!=(const Fixed& obj) const;
+        bool operator<(const Fixed& obj) const;
+        bool operator>(const Fixed& obj) const;
+        bool operator<=(const Fixed& obj) const;
+        bool operator>=(const Fixed& obj) const;
+
+        Fixed operator=(const Fixed& obj);
+	    Fixed operator+(const Fixed& obj);
+	    Fixed operator*(const Fixed& obj);
+	    Fixed operator/(const Fixed& obj);
+	    Fixed operator-(const Fixed& obj);
+        int getRawBits(void) const;
+        float toFloat( void ) const;
+        int toInt( void ) const;
+};
+std::ostream &operator<<(std::ostream& os, const Fixed& dt);
+
+#endif
