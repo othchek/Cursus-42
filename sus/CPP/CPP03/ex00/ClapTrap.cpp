50a5635d23a1a4d814de10feaf83d7a3081fa635
Othman Chekairi <otchekai@e2r9p6.1337.ma>
2023-10-26 21:25:15 +0100

    hh

--- sus/CPP/CPP03/ex00/ClapTrap.cpp
+++ sus/CPP/CPP03/ex00/ClapTrap.cpp
@@ -1,31 +1,30 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap() {
-
 }
 
-ClapTrap::ClapTrap(std::string name) 
+ClapTrap::ClapTrap(std::string Name) 
 {
     std::cout << "Constructor called" << std::endl;
-    Name = name;
-    Attackdamage = 10;
+    this->Name = Name;
+    Hitpoints = 10;
     Energypoints = 10;
     Attackdamage = 0;
 }
 
 void ClapTrap::attack(const std::string& target) {
     if (Energypoints == 0) {
-        std::cout << "This ClapTrap " << Name << " could not attack right neow" << std::endl;
+        std::cout << "This ClapTrap could not attack right neow" << std::endl;
         return ;
     }
-    std::cout << "this ClapTrap " << Name << " attacked " << target << " for this amount of: " << Attackdamage << " of damage" << std::endl;
+    std::cout << "this ClapTrap " << Name << " attacked " << target << " for this amount: " << Attackdamage << " of damage" << std::endl;
     Energypoints--;
     Hitpoints -= Attackdamage;
 }
 
 void ClapTrap::takeDamage(unsigned int amount) {
     if (Hitpoints < 1) {
-        std::cout << "This ClapTrap " << Name << " is Deadge how unfortunate" << std::endl;
+        std::cout << "This ClapTrap is Deadge how unfortunate" << std::endl;
         Hitpoints = 0;
         return ;
     }
@@ -33,8 +32,8 @@ void ClapTrap::takeDamage(unsigned int amount) {
 }
 
 void ClapTrap::beRepaired(unsigned int amount) {
-    if (Hitpoints == 0 || Hitpoints == 0) {
-        std::cout << "This ClapTrap " << Name << " Cant be Repaired" << std::endl;
+    if (Hitpoints == 0 || Energypoints == 0) {
+        std::cout << "This ClapTrap Cant be Repaired" << std::endl;
         return ;
     }
     std::cout << "This ClapTrap has been repaired for this amount: " << amount << std::endl;
--- sus/CPP/CPP03/ex00/main.cpp
+++ sus/CPP/CPP03/ex00/main.cpp
@@ -1,17 +1,11 @@
 #include "ClapTrap.hpp"
 
 int main() {
-  	ClapTrap john("John");
-	ClapTrap jim("Jim");
-	ClapTrap joe;
+  	ClapTrap john("abmoula");
 
-	john.attack("zac");
-	john.attack("james");
-	john.attack("simo");
-	john.takeDamage(5);
-	jim.takeDamage(9);
-	jim.takeDamage(10);
-	joe.beRepaired(10);
-	joe.takeDamage(19);
-	joe.attack("mohammed");
+	john.attack("sma3ine");
+    john.takeDamage(5);
+    john.takeDamage(5);
+    john.takeDamage(5);
+    john.takeDamage(5);
 }
