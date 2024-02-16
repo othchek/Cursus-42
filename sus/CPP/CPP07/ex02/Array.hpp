e68a1b54f2e9ebeefd598d62394a0e16aa1024de
Othman Chekairi <otchekai@e2r6p7.1337.ma>
2024-02-16 16:28:36 +0100

    hh

--- sus/CPP/CPP07/ex02/Array.hpp
+++ sus/CPP/CPP07/ex02/Array.hpp
@@ -5,14 +5,42 @@ template <typename T>
 class Array
 {
 	private :
-		T *data;
+		T 	  *data;
+		size_t length;
 	public :
-		Array(void) {data = new T[0]}
-		Array(unsigned int n) {data = new T[n];}
-		Array(Array const &copy);
-		~Array(void);
-		void	size(void);
-		Array const &operator = (Array const &rhs);
+		Array(void) {
+			data = new T[0],
+			length = 0;
+		};
+		Array(unsigned int n) {data = new T[n], length = n;}
+		class sub : public std::exception {
+			public :
+				const char *what() const throw() {
+					return ("Error Out of Bounds");
+				}
+		};
+		Array(Array const &copy) {
+			*this = copy;
+		};
+		~Array(void) {};
+		size_t	size(void) {
+			return (length);
+		};
+		Array const &operator = (Array const &rhs) {
+			data = rhs.data;
+			length = rhs.length;
+			return *this;
+		};
+		T& operator[]( unsigned int i ) {
+			if (i >= length)
+				throw (sub());
+			return (data[i]);
+		}
+		const T &operator[]( unsigned int i ) const {
+			if (i >= length)
+				throw (sub());
+			return (data[i]);
+		}
 };
 
 #endif
